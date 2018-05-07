1408239edaafa99449ac40a56655996b82538b4a
Robin Malhotra <me@rmalhotra.com>
2018-05-07 12:54:22 +0530

    Setup skeleton for Contact Header
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

--- GoJekTakeHome/ContactDetailsViewController.swift
+++ GoJekTakeHome/ContactDetailsViewController.swift
@@ -19,27 +19,60 @@ class ContactHeaderView: UIView {
 	let profilePicImageView = UIImageView()
 	let nameLabel = UILabel()
 
+	let favoriteButton: ContactFunctionButton
+
+	let stackView = UIStackView()
+
+	let buttonStack = UIStackView()
+
+	let flaggyFavoriteButton = FlaggyButton(trueImage: #imageLiteral(resourceName: "Favorite"), falseImage: #imageLiteral(resourceName: "Unfavorite"))
+	let otherButtons: [ContactFunctionButton] = {
+		let phoneButton = UIButton()
+		phoneButton.setImage(#imageLiteral(resourceName: "Placeholder"), for: .normal)
+
+		let messageButton = UIButton()
+		messageButton.setImage(#imageLiteral(resourceName: "Placeholder"), for: .normal)
+
+		let emailButton = UIButton()
+		emailButton.setImage(#imageLiteral(resourceName: "Placeholder"), for: .normal)
+
+		let phone = ContactFunctionButton(button: phoneButton, text: "Call")
+		let message = ContactFunctionButton(button: messageButton, text: "Message")
+		let email = ContactFunctionButton(button: emailButton, text: "Email")
+
+		return [phone, message, email]
+	}()
+
 	override init(frame: CGRect) {
+		self.favoriteButton = ContactFunctionButton.init(button: flaggyFavoriteButton, text: "Favorite")
 		super.init(frame: frame)
-		self.addSubview(profilePicImageView)
-		self.addSubview(nameLabel)
+		self.addSubview(stackView)
+
+		otherButtons.forEach(buttonStack.addArrangedSubview(_:))
+		buttonStack.addArrangedSubview(favoriteButton)
+
+		stackView.addArrangedSubview(profilePicImageView)
+		stackView.addArrangedSubview(nameLabel)
+		stackView.addArrangedSubview(buttonStack)
+		buttonStack.distribution = .fillEqually
+
+		stackView.alignment = .center
+
+		stackView.axis = .vertical
+		stackView.alignEdges(to: self)
+		stackView.translatesAutoresizingMaskIntoConstraints = false
+		NSLayoutConstraint.activate([
+			profilePicImageView.widthAnchor.constraint(equalToConstant: 120),
+			profilePicImageView.heightAnchor.constraint(equalToConstant: 120)
+		])
+	}
+
+	func configure(name: String, image: UIImage) {
+		profilePicImageView.image = image
+		nameLabel.text = name
 	}
 
 
-
-	required init?(coder aDecoder: NSCoder) {
-		fatalError("init(coder:) has not been implemented")
-	}
-
-}
-
-class ContactFunctionButtons: UIView {
-
-	let stack = UIStackView()
-
-	override init(frame: CGRect) {
-		super.init(frame: frame)
-	}
 
 	required init?(coder aDecoder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
@@ -51,10 +84,26 @@ class ContactFunctionButtons: UIView {
 class ContactFunctionButton: UIView {
 	let label = UILabel()
 	let button: UIButton
+	let stackView = UIStackView()
 
-	init(button: UIButton) {
+	init(button: UIButton, text: String) {
 		self.button = button
 		super.init(frame: .zero)
+
+		label.text = text
+		stackView.addArrangedSubview(button)
+		stackView.addArrangedSubview(label)
+		stackView.axis = .vertical
+		stackView.alignment = .center
+		self.addSubview(stackView)
+
+		stackView.translatesAutoresizingMaskIntoConstraints = false
+		stackView.alignEdges(to: self)
+
+	}
+
+	override var intrinsicContentSize: CGSize {
+		return stackView.intrinsicContentSize
 	}
 
 	required init?(coder aDecoder: NSCoder) {
@@ -94,7 +143,7 @@ class FlaggyButton: UIButton {
 	}
 
 	override var intrinsicContentSize: CGSize {
-		return CGSize.init(width: 44.0, height: 44.0)
+		return CGSize(width: 44.0, height: 44.0)
 	}
 
 	required init?(coder aDecoder: NSCoder) {
@@ -113,22 +162,24 @@ class ContactDetailsViewController: UIViewController {
 		fatalError("init(coder:) has not been implemented")
 	}
 
-	let flaggyButton = FlaggyButton(trueImage: #imageLiteral(resourceName: "Favorite"), falseImage: #imageLiteral(resourceName: "Unfavorite"))
+	let contactHeaderView = ContactHeaderView(frame: .zero)
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-		self.view.addSubview(flaggyButton)
-
 		self.view.backgroundColor = .white
 
-		self.flaggyButton.tintColor = .red
+		self.view.addSubview(contactHeaderView)
+		contactHeaderView.translatesAutoresizingMaskIntoConstraints = false
+		contactHeaderView.alignEdges(to: self.view, insets: UIEdgeInsets.init(top: 0, left: 0, bottom: 300, right: 0))
+		contactHeaderView.configure(name: "asdkfnkadsjfnkjdsnf", image: #imageLiteral(resourceName: "Placeholder"))
 
         // Do any additional setup after loading the view.
     }
 
 	override func viewDidLayoutSubviews() {
 		super.viewDidLayoutSubviews()
-		flaggyButton.frame = CGRect.init(x: 100, y: 100, width: 44, height: 44)
 	}
 
     override func didReceiveMemoryWarning() {
